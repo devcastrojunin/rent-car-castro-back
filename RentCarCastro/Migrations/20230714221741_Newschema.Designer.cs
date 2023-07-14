3a21d6cb3e89589c9959add2a11d8853320a7c78
devcastrojunin <devcastrojunin@gmail.com>
2023-07-14 19:26:17 -0300

    feat: update schema

--- RentCarCastro/Migrations/20230714221741_Newschema.Designer.cs
+++ RentCarCastro/Migrations/20230714221741_Newschema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentCarCastro.Data;
 
@@ -11,9 +12,10 @@ using RentCarCastro.Data;
 namespace RentCarCastro.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230714221741_Newschema")]
+    partial class Newschema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RentCarCastro/Migrations/20230714221741_Newschema.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentCarCastro.Migrations
+{
+    public partial class Newschema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 35063373);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 942662159);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2041960455);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CNPJ", "CPF", "Email", "IsActive", "Name", "Password", "UserName" },
+                values: new object[] { 1120623263, "", "27904525801", "teste@teste3.com", false, "Maria Cecília", "admin@123", "maria.cecilia" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CNPJ", "CPF", "Email", "IsActive", "Name", "Password", "UserName" },
+                values: new object[] { 1189301055, "", "27904525801", "teste@teste.com", true, "Junior Castro", "admin@123", "junior.castro" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CNPJ", "CPF", "Email", "IsActive", "Name", "Password", "UserName" },
+                values: new object[] { 1532583414, "", "27904525801", "teste@teste2.com", true, "José da Silva", "admin@123", "jose.silva" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1120623263);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1189301055);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1532583414);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CNPJ", "CPF", "Email", "IsActive", "Name", "Password", "UserName" },
+                values: new object[] { 35063373, "", "27904525801", "teste@teste.com", true, "Junior Castro", "admin@123", "junior.castro" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CNPJ", "CPF", "Email", "IsActive", "Name", "Password", "UserName" },
+                values: new object[] { 942662159, "", "27904525801", "teste@teste3.com", false, "Maria Cecília", "admin@123", "maria.cecilia" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CNPJ", "CPF", "Email", "IsActive", "Name", "Password", "UserName" },
+                values: new object[] { 2041960455, "", "27904525801", "teste@teste2.com", true, "José da Silva", "admin@123", "jose.silva" });
+        }
+    }
+}
