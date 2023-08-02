995cb5d43a6a5cce37acf6c82e0ade6090568931
devcastrojunin <devcastrojunin@gmail.com>
2023-08-02 10:52:30 -0300

    feat: new enum added

---
+++ RentCarCastro/Enums/RoleNameEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace RentCarCastro.Enums
+{
+    public enum RoleNameEnum
+    {
+        [Description("A")]
+        ADMIN,
+        [Description("R")]
+        READER
+    }
+}
